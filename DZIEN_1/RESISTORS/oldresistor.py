736a1e11805a335537c707cb883b4198585ea66f
albim72 <marcin.albiniak72@gmail.com>
2023-05-15 11:42:21 +0200

    Update oldresistor.py

--- DZIEN_1/RESISTORS/oldresistor.py
+++ DZIEN_1/RESISTORS/oldresistor.py
@@ -1,9 +1,12 @@
 class OldResistor:
     def __init__(self,ohms):
         self._ohms = ohms
-        
+
+    def __repr__(self):
+        return f"opornik o oporze początkowym: {self._ohms} omów"
+    
     def get_ohms(self):
         return self._ohms
-    
+
     def set_ohms(self,ohms):
         self._ohms = ohms
