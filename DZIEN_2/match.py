a6391ef3c9415f6c80e7e02d09356e0fb3408a79
albim72 <marcin.albiniak72@gmail.com>
2023-05-16 10:26:27 +0200

    Create match.py

---
+++ DZIEN_2/match.py
@@ -0,0 +1,21 @@
+dzien = 'sobota'
+
+match dzien:
+    case 'poniedziałek' :
+        print("pierwszy dzień tygodnia!")
+    case 'wtorek':
+        print("drugi dzień tygodnia!")
+    case 'środa':
+        print("środek tygodnia!")
+    case 'czwartek':
+        print("czwarty dzień tygodnia!")
+    case 'piątek':
+        print("już piąty dzień tygodnia!")
+    case 'sobota':
+        print("to już szósty dzień!")
+    case 'sobota':
+        print("wielka impreza!")
+    case 'niedziela':
+        print("odpoczynek!")
+    case blabla:
+        print("nie ma takiego dnia!")
