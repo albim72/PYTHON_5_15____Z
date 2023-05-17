54d2c679387543673055e7d710fb5d590eeccdd1
albim72 <marcin.albiniak72@gmail.com>
2023-05-17 10:08:25 +0200

    Update systemop_fasada.py

--- DZIEN_3/systemop_fasada.py
+++ DZIEN_3/systemop_fasada.py
@@ -45,8 +45,8 @@ class FileServer(Server):
 
     def create_file(self,user,name,permissions):
         print(f"próba utworzenia pliku {name} dla użytkownika {user} z uprawnieniami: {permissions}.")
-        
-        
+
+
 class ProcessServer(Server):
 
     def __init__(self):
@@ -63,4 +63,32 @@ class ProcessServer(Server):
 
     def create_process(self,user,name):
         print(f"próba utworzenia procesu {name} dla użytkownika {user}.")
-        
+
+class WindowsServer:
+    pass
+
+class NetworkServer:
+    pass
+
+class OperatingSystem:
+    def __init__(self):
+        self.fs = FileServer()
+        self.ps = ProcessServer()
+
+    def start(self):
+        [i.boot() for i in (self.fs,self.ps)]
+
+    def create_file(self,user,name,permissions):
+        return self.fs.create_file(user,name,permissions)
+
+    def create_process(self,user,name):
+        return self.ps.create_process(user,name)
+
+def main():
+    os = OperatingSystem()
+    os.start()
+    os.create_file('nowy','hello','-rw-r-r')
+    os.create_process('big','ls/tmp')
+
+if __name__ == '__main__':
+    main()
