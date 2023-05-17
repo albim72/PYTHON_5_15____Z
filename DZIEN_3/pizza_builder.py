08d0b1daaee703c1616d530a45b0c9882600d283
albim72 <marcin.albiniak72@gmail.com>
2023-05-17 08:45:07 +0200

    Update pizza_builder.py

--- DZIEN_3/pizza_builder.py
+++ DZIEN_3/pizza_builder.py
@@ -93,3 +93,31 @@ class CreamyBaconBuilder:
         time.sleep(self.baking_time)
         self.progress = PizzaProgress.ready
         print('your creamy bacon is ready!')
+
+class Waiter:
+    def __init__(self):
+        self.builder = None
+
+    def construct_pizza(self,builder):
+        self.builder = builder
+        steps = (builder.prepare_dough,
+                 builder.add_sauce,
+                 builder.add_topping,
+                 builder.bake)
+        [step() for step in steps]
+
+    @property
+    def pizza(self):
+        return self.builder.pizza
+
+def validate_style(builders):
+    try:
+        input_msg = 'What pizza would you like, [m]argarita or [c]reamy bacon?'
+        pizza_style = input(input_msg)
+        builder =  builders[pizza_style]()
+        valid_input = True
+    except KeyError:
+        error_msg = 'Sorry, only margarita (key m) and creamy bacon (key c) are avaliable'
+        print(error_msg)
+        return (False,None)
+    return (True,builder)
