ac6e9ca1cd3e7561b03e24b7e5381f7ea2967f82
albim72 <marcin.albiniak72@gmail.com>
2023-05-17 09:09:54 +0200

    Update abstract_factory_game.py

--- DZIEN_3/abstract_factory_game.py
+++ DZIEN_3/abstract_factory_game.py
@@ -2,33 +2,70 @@
 class Frog:
     def __init__(self,name):
         self.name = name
-        
+
     def __str__(self):
         return self.name
-    
+
     def interact_with(self,obstacle):
         act = obstacle.action()
         msg = f'Żaba {self} spotyka {obstacle} i {act}'
         print(msg)
-        
+
 class Bug:
     def __str__(self):
-        return 'robak'
-    
+        return 'robaka'
+
     def action(self):
         return 'zjada go'
-    
-    
+
+
 class FrogWorld:
     def __init__(self,name):
         print(self)
         self.player_name = name
-        
+
     def __str__(self):
         return '\n\n\t---------- Frog World --------------'
-    
+
     def make_character(self):
         return Frog(self.player_name)
     
     def make_obstacle(self):
         return Bug()
+
+
+# Warlock game
+class Warlock:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def interact_with(self, obstacle):
+        act = obstacle.action()
+        msg = f'Czarnoksiężnik {self} walczy ze {obstacle} i {act}'
+        print(msg)
+
+
+class Ork:
+    def __str__(self):
+        return 'złym orkiem'
+
+    def action(self):
+        return 'masakruje go i wysyła do zaświatów'
+
+
+class WarlockWorld:
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+
+    def __str__(self):
+        return '\n\n\t---------- Warlock World --------------'
+
+    def make_character(self):
+        return Warlock(self.player_name)
+
+    def make_obstacle(self):
+        return Ork()
